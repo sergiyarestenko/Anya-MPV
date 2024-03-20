34451317d363dc48b64ba3e0b076a837e35bc1a6
arestenkos <arestenkos@gmail.com>
2024-03-20 14:22:23 +0200

    linters

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+
+  ],
+  plugins: ['prettier'],
+
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
+
