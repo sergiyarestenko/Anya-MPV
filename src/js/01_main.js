de3f958730e5b2dec34d5a253c1df38c649d8641
arestenkos <arestenkos@gmail.com>
2024-04-03 17:16:47 +0300

    headerObserver

---
+++ src/js/01_main.js
@@ -0,0 +1,31 @@
+// function debounce(func, ms = 250) {
+//   let timeout
+//   return function () {
+//     clearTimeout(timeout)
+//     timeout = setTimeout(() => func.apply(this, arguments), ms)
+//   }
+// }
+
+const observeHeader = (header, observeTarget) => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        header.classList.remove('scrolled')
+      } else {
+        header.classList.add('scrolled')
+      }
+    })
+  })
+  observer.observe(observeTarget)
+}
+
+const changeHeaderClassOnScroll = () => {
+  const header = document.getElementById('header')
+  const observeTarget = document.getElementById('observer')
+
+  observeHeader(header, observeTarget)
+}
+
+;(function () {
+  changeHeaderClassOnScroll();
+})()
